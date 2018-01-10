c9d24bff814b874a59766f1f5fcd49355ac3109f
MaciejRozmiarek <maciej.rozmiarek@gmail.com>
2018-01-10 07:20:58 +0100

    dwa

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -32,18 +33,24 @@ namespace Client
             int port = 5000;
             //string localsddress = "127.0.0.1";
 
-            DataSet DS = new DataSet();
-            XmlSerializer xs = new XmlSerializer(typeof(DataSet));
-
+            //DataSet DS = new DataSet();
+            //XmlSerializer xs = new XmlSerializer(typeof(DataSet));
+            BinaryFormatter bf = new BinaryFormatter();
             TcpListener listener = new TcpListener(port);
             listener.Start();
            
 
             TcpClient client = listener.AcceptTcpClient();
-            Stream stream = client.GetStream();
-            DS = (DataSet)xs.Deserialize(stream);
-            stream.Close();
-            client.Close();
+            /*MemoryStream stream = new MemoryStream();
+            stream.Position = 0;
+            DS = (DataSet)bf.Deserialize(stream);
+            stream.Close();*/
+            MemoryStream ms = new MemoryStream();
+            BinaryFormatter binForm = new BinaryFormatter();
+            ms.Write(ms.ToArray(), 0, ms.ToArray().Length);
+            ms.Seek(0, SeekOrigin.Begin);
+            DataSet DS = (DataSet)bf.Deserialize(ms);
+            //client.Close();
             //listener.Close();
 
             dataGridView1.DataSource = DS.Tables[0].DefaultView;
